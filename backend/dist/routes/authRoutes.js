f1965a414610374569ab50b54957317d9c6f627c
prashant-kumar96 <prashant.kumar560@gmail.com>
2024-11-05 17:13:07 +0530

    latest code

--- backend/dist/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { login, me, register } from "../controllers/authController.js";
-import verifyToken from "../middleware/authenticate.js";
-const router = express.Router();
-router.post("/register", register);
-router.post("/login", login);
-router.get("/me", verifyToken, me);
-export default router;
