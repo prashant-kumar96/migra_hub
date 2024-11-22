cae4bf8454d5c2c558c9b9189d81e4beece2f78e
Bavneet Kaur <bavneetgulab@gmail.com>
2024-11-22 17:38:15 +0530

    created reusable components for table and create buttons and tags and updated case Mangers page and table

--- frontend/src/pages/caseManagerPage.tsx
+++ frontend/src/pages/caseManagerPage.tsx
@@ -2,17 +2,41 @@ import React, { useEffect, useState } from "react";
 import AfterLoginLayout from "../components/afterLoginLayout/AfterLoginLayout";
 import CreateCaseManager from "@/components/CreateCaseManager";
 import { getCaseManagers } from "@/api/auth";
-import CaseManagerTable from "@/components/CaseManagerTable";
+import Table from "@/components/ui/Table";
+import NATag from "@/components/ui/tags/NATag";
+import Role from "@/components/ui/tags/Role";
 
 const CaseManagerPage = () => {
-  const [caseManagersList, setCaseManagersList] = useState();
+  const [caseManagersList, setCaseManagersList] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const headers = ["Name", "Email", "Role"];
 
   const getCaseManagersFunction = async () => {
-    // Fetch case managers data from the API here
-    const result = await getCaseManagers();
-    console.log("result getCaseManagers", result?.data?.user);
-    setCaseManagersList(result?.data?.user);
+    try {
+      const result = await getCaseManagers();
+      console.log("result getCaseManagers", result?.data?.user);
+
+      const formattedData =
+        result?.data?.user?.map((user) => ({
+          ...user,
+          name: user.name
+            ? user.name.charAt(0).toUpperCase() +
+              user.name.slice(1).toLowerCase()
+            : <NATag/>,
+            role: user.role ? (
+              <Role
+                text={user.role.replace(/[^a-zA-Z0-9 ]/g, " ").toLowerCase()}
+                
+              />
+            ) : (
+              <NATag />)
+        })) || [];
+
+      setCaseManagersList(formattedData);
+    } catch (error) {
+      console.error("Error fetching case managers:", error);
+      setCaseManagersList([]);
+    }
   };
 
   // Fetch case managers data on component mount
@@ -26,7 +50,7 @@ const CaseManagerPage = () => {
         setIsModalOpen={setIsModalOpen}
         isModalOpen={isModalOpen}
       />
-      <CaseManagerTable data={caseManagersList} />
+      <Table data={caseManagersList} headers={headers} />
     </div>
   );
 };
--- frontend/src/pages/test3.tsx
+++ frontend/src/pages/test3.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import { FaUserTie } from "react-icons/fa6";
-const GithubButton = () => {
-  return (
-    <button className="flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-3 rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-gray-500 hover:border-gray-800 hover:from-black hover:to-gray-900">
-      <FaUserTie size={20}/>
-      <span className='text-lg font-thin uppercase tracking-wide'>Create Case Manager</span>
-    </button>
-  );
+import Table from "@/components/ui/table";
+import React from "react";
+
+
+const ProductTable = () => {
+  const headers = ["Product name", "Color", "Category", "Price", "Action"];
+  const data = [
+    {
+      "product name": "Apple MacBook Pro 17\"",
+      color: "Silver",
+      category: "Laptop",
+      price: "$2999",
+    },
+    {
+      "product name": "Microsoft Surface Pro",
+      color: "White",
+      category: "Laptop PC",
+      price: "$1999",
+    },
+    {
+      "product name": "Magic Mouse 2",
+      color: "Black",
+      category: "Accessories",
+      price: "$99",
+    },
+    {
+      "product name": "Google Pixel Phone",
+      color: "Gray",
+      category: "Phone",
+      price: "$799",
+    },
+    {
+      "product name": "Apple Watch 5",
+      color: "Red",
+      category: "Wearables",
+      price: "$999",
+    },
+  ];
+
+  return <Table headers={headers} data={data} />;
 };
 
-export default GithubButton;
+export default ProductTable;
