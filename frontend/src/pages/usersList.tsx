b9860b7b91d528ee6520ebdb4138d5adfca2fd75
Bavneet Kaur <bavneetgulab@gmail.com>
2024-11-25 09:57:47 +0530

    updated users table @admin's side

--- frontend/src/pages/usersList.tsx
+++ frontend/src/pages/usersList.tsx
@@ -1,17 +1,42 @@
 import React, { useEffect, useState } from "react";
 import AfterLoginLayout from "../components/afterLoginLayout/AfterLoginLayout";
 import { getAllUsersWhoHaveDonePayment } from "@/api/auth";
+import NATag from "@/components/ui/tags/NATag";
+import Role from "@/components/ui/tags/Role";
+import Table from "@/components/ui/Table";
 
 const UsersList = () => {
   const [usersData, setUsersData] = useState();
+  const headers = ["Name", "Email", "Role", "Payment"];
 
   const getAllUsersWhoHaveDonePaymentFunction = async () => {
-    const result = await getAllUsersWhoHaveDonePayment();
-    console.log("result getAllUsersWhoHaveDonePaymentFunction", result);
-    setUsersData(result?.data?.user);
+    try {
+      const result = await getAllUsersWhoHaveDonePayment();
+      console.log("result getAllUsersWhoHaveDonePaymentFunction", result);
+      const formattedData =
+        result?.data?.user?.map((user) => ({
+          ...user,
+          name: user.name ? (
+            user.name.charAt(0).toUpperCase() + user.name.slice(1).toLowerCase()
+          ) : (
+            <NATag />
+          ),
+          role: user.role ? (
+            <Role
+              text={user.role.replace(/[^a-zA-Z0-9 ]/g, " ").toLowerCase()}
+              bgClass="bg-lime-100"
+              textColor="text-green-600"
+            />
+          ) : (
+            <NATag />
+          ),
+        })) || [];
+      setUsersData(formattedData);
+    } catch (error) {
+      console.error("Error fetching case managers:", error);
+      setUsersData([]);
+    }
   };
-
-  console.log("usersData", usersData);
 
   useEffect(() => {
     getAllUsersWhoHaveDonePaymentFunction();
@@ -19,61 +44,9 @@ const UsersList = () => {
 
   return (
     <div>
-      User List
-      <div>
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead>
-            <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Name
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Email
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Role
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                IsPaymentDone
-              </th>
-              {/* <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Action
-              </th> */}
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {usersData?.map((user, index) => (
-              <tr>
-                <td className="px-6 py-4 whitespace-nowrap">{user.name}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
-                <td className="px-6 py-4 whitespace-nowrap">User</td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
-                    {user.isStripePaymentDone ? "Yes" : "No"}
-                  </span>
-                </td>
-                {/* <td className="px-6 py-4 whitespace-nowrap">
-                <button className="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">
-                  Edit
-                </button>
-                <button className="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">
-                  Delete
-                </button>
-              </td> */}
-              </tr>
-            ))}
-            <tr>
-              {/* <td className="px-6 py-4 whitespace-nowrap">
-                <button className="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">
-                  Edit
-                </button>
-                <button className="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">
-                  Delete
-                </button>
-              </td> */}
-            </tr>
-          </tbody>
-        </table>
+      <span className="text-Indigo ml-6">User List</span>
+      <div className="relative overflow-auto ">
+        <Table data={usersData} headers={headers} />
       </div>
     </div>
   );
