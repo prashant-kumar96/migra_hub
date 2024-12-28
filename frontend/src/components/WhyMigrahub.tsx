987ab02d08152602de6d5d1ef7c6002976c11d79
vinender <vinendersingh91@gmail.com>
2024-12-28 17:09:58 +0530

    icon resized

--- frontend/src/components/WhyMigrahub.tsx
+++ frontend/src/components/WhyMigrahub.tsx
@@ -4,29 +4,30 @@ const WhyTrustMigraHub = () => {
   const features = [
     {
       icon: '/imgs/icons/1.png',
-      title: 'Speed and Simplicity',
+      title: 'Seamless Convenience',
       description:
-        'Easy, traveler-friendly application process. Simple and much less complicated than dealing with foreign governments.',
+        'Say goodbye to tedious paperwork! We handle everything for you at an exceptionally affordable price, letting you focus on what truly matters—your journey.',
     },
     {
       icon: '/imgs/icons/2.png',
-      title: 'Expert Quality Check',
+      title: 'Unmatched Speed',
       description:
-        'All documents are reviewed by a team of immigration experts. Our staff is well trained and offers years of experience.',
+        'Our expedited service ensures a swift and efficient application process. Once we receive all required details and documents, we’ll submit your application online within just 48 hours.',
     },
     {
       icon: '/imgs/icons/4.png',
-      title: 'Secure and Safe',
+      title: 'Unrivaled Quality',
       description:
-        'World-class data centers and state-of-the-art security. Your credit card information will never be exposed to any government websites!',
+        'Your application is reviewed by our elite in-house team of licensed RCCIC professionals and experienced immigration lawyers, minimizing risks and maximizing success.',
     },
     {
       icon: '/imgs/icons/3.png',
-      title: 'Awesome Support',
+      title: 'Dedicated Support',
       description:
-        'Our best in class customer service team is here to help you. We want you to enjoy your travels and avoid the stress of getting a visa!',
+        'From day one, you’ll be assigned a personal Case Manager who will guide you through every step of the process, ensuring a smooth and stress-free experience.',
     },
   ];
+  
 
   return (
     <div className="py-10">
@@ -36,7 +37,7 @@ const WhyTrustMigraHub = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-6 lg:px-20">
         {features.map((feature, index) => (
           <div key={index} className="bg-white shadow-xl rounded-lg p-4 text-center">
-            <img src ={feature.icon} className="text-6xl mx-auto mb-2 w-[80px] h-auto"/>
+          <div className='h-24'> <img src ={feature.icon} className="text-6xl mx-auto mb-2 w-[80px] h-auto"/></div>
             <h3 className="text-xl text-DarkGray font-greycliff font-bold mb-2">{feature.title}</h3>
             <p className="text-Gray mt-2 text-[17px] tracking-wide font-thin">{feature.description}</p>
           </div>
